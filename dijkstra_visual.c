411239024c972d063c8bb64cdc04b4664cabab5e
SabbaJohn <victorsabba@gmail.com>
2023-09-12 16:01:01 -0300

    formating

--- dijkstra_visual.c
+++ dijkstra_visual.c
@@ -2,7 +2,7 @@
 /*
  * Author: John. Sabbá
  * Date: September / 2023
- 
+
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation.
@@ -35,128 +35,146 @@
 #endif
 #define INFINITY 9999
 
-typedef struct{
+typedef struct
+{
     int x;
     int y;
 } Point;
 
-
 // Structure to represent a vertex in the graph
-typedef struct {
+typedef struct
+{
     int distance;
     bool visited;
     Point point;
 } Vertex;
 
 // Function to find the vertex with the minimum distance
-int minDistance(Vertex *vertices, int numVertices) {
+int minDistance(Vertex *vertices, int numVertices)
+{
     int min = INFINITY;
     int minIndex;
-    
-    for (int v = 0; v < numVertices; v++) {
-        if (!vertices[v].visited && vertices[v].distance <= min) {
+
+    for (int v = 0; v < numVertices; v++)
+    {
+        if (!vertices[v].visited && vertices[v].distance <= min)
+        {
             min = vertices[v].distance;
             minIndex = v;
         }
     }
-    
+
     return minIndex;
 }
 
 // Function to print the shortest path from source to destination
-void printShortestPath(int *parent, int dest, Vertex *vertices, SDL_Renderer* renderer) {
+void printShortestPath(int *parent, int dest, Vertex *vertices, SDL_Renderer *renderer)
+{
     SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Shortest path color
-    if (parent[dest] == -1) {
+    if (parent[dest] == -1)
+    {
         printf("%d ", dest);
         return;
     }
-    
+
     SDL_RenderDrawLine(renderer, vertices[dest].point.x, vertices[dest].point.y, vertices[parent[dest]].point.x, vertices[parent[dest]].point.y);
     SDL_RenderPresent(renderer);
     usleep(100000);
-    printShortestPath(parent, parent[dest],vertices, renderer);
+    printShortestPath(parent, parent[dest], vertices, renderer);
     printf("%d ", dest);
 }
 
 // Function to implement Dijkstra's shortest path algorithm
-void dijkstra(int graph[MAX_VERTICES][MAX_VERTICES], int numVertices, int src, int dest, SDL_Renderer* renderer) {
+void dijkstra(int graph[MAX_VERTICES][MAX_VERTICES], int numVertices, int src, int dest, SDL_Renderer *renderer)
+{
     Vertex vertices[MAX_VERTICES];
     int parent[MAX_VERTICES];
     TTF_Init();
-    TTF_Font* font = TTF_OpenFont("Arial.ttf", 24);
+    TTF_Font *font = TTF_OpenFont("Arial.ttf", 24);
     char str[20];
-    
+
     // Initialize all vertices with maximum distance and not visited
-    for (int i = 0; i < numVertices; i++) {
+    for (int i = 0; i < numVertices; i++)
+    {
         vertices[i].distance = INFINITY;
         vertices[i].visited = false;
-        vertices[i].point.x = rand() %1000 + 50;
-        vertices[i].point.y = rand() %640 + 50;
+        vertices[i].point.x = rand() % 1000 + 50;
+        vertices[i].point.y = rand() % 640 + 50;
         parent[i] = -1;
         SDL_Color textColor;
-        if (i == src){
-            textColor = (SDL_Color) {0, 255, 0, 255};
-        } else if(i == dest) {
-            textColor = (SDL_Color) {255, 0, 0, 255};
-        } else {
-            textColor = (SDL_Color) {255, 255, 255, 255};
+        if (i == src)
+        {
+            textColor = (SDL_Color){0, 255, 0, 255};
         }
-        
+        else if (i == dest)
+        {
+            textColor = (SDL_Color){255, 0, 0, 255};
+        }
+        else
+        {
+            textColor = (SDL_Color){255, 255, 255, 255};
+        }
+
         SDL_Rect rect = {vertices[i].point.x, vertices[i].point.y, 30, 30};
-        
+
         sprintf(str, "%d", i);
-        SDL_Surface* textSurface = TTF_RenderText_Solid(font, str, textColor);
-        SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+        SDL_Surface *textSurface = TTF_RenderText_Solid(font, str, textColor);
+        SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
         SDL_RenderCopyEx(renderer, textTexture, NULL, &rect, 0, NULL, SDL_FLIP_NONE);
 
         SDL_FreeSurface(textSurface);
         SDL_DestroyTexture(textTexture);
         SDL_RenderPresent(renderer);
     }
-    
+
     // Distance from source to itself is always 0
     vertices[src].distance = 0;
-    
+
     // Find shortest path for all vertices
-    for (int count = 0; count < numVertices - 1; count++) {
+    for (int count = 0; count < numVertices - 1; count++)
+    {
         int u = minDistance(vertices, numVertices);
-        
+
         vertices[u].visited = true;
 
-        if (u != dest && u != src){
+        if (u != dest && u != src)
+        {
             SDL_SetRenderDrawColor(renderer, 255, 165, 0, 127); // Color of visited vertex
-            
-        }else if(u == dest){
+        }
+        else if (u == dest)
+        {
             SDL_SetRenderDrawColor(renderer, 255, 0, 0, 127); // Color of destination vertex
-        }else if (u == src){
+        }
+        else if (u == src)
+        {
             SDL_SetRenderDrawColor(renderer, 0, 255, 0, 127); // Color of source vertex
         }
         SDL_Rect rect = {vertices[u].point.x, vertices[u].point.y, 10, 10};
         SDL_RenderFillRect(renderer, &rect);
         SDL_RenderPresent(renderer);
-        
-        for (int v = 0; v < numVertices; v++) {
-            if (!vertices[v].visited && graph[u][v] && vertices[u].distance + graph[u][v] < vertices[v].distance) {
+
+        for (int v = 0; v < numVertices; v++)
+        {
+            if (!vertices[v].visited && graph[u][v] && vertices[u].distance + graph[u][v] < vertices[v].distance)
+            {
                 vertices[v].distance = vertices[u].distance + graph[u][v];
                 parent[v] = u;
                 SDL_SetRenderDrawColor(renderer, 255, 255, 255, 50);
                 SDL_RenderDrawLine(renderer, vertices[v].point.x, vertices[v].point.y, vertices[u].point.x, vertices[u].point.y);
             }
-            
+
             SDL_RenderPresent(renderer);
             usleep(500);
         }
     }
-    
+
     printf("Caminho mais curto de %d para %d: ", src, dest);
     printShortestPath(parent, dest, vertices, renderer);
     printf("\n");
 }
 
-
-
-
-int main() {
+int main()
+{
     int graph[MAX_VERTICES][MAX_VERTICES];
     int numVertices;
     int src, dest;
@@ -168,46 +186,60 @@ int main() {
     int option;
     scanf("%d", &option);
 
-    if (option == 1) {
+    if (option == 1)
+    {
         // Use pre-defined graph
-        int predefinedGraph[9][9] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-                                      { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-                                      { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-                                      { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-                                      { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-                                      { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-                                      { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-                                      { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-                                      { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+        int predefinedGraph[9][9] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                                     {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                                     {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                                     {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                                     {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                                     {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                                     {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                                     {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                                     {0, 0, 2, 0, 0, 0, 6, 7, 0}};
         numVertices = 9;
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++) {
+        for (int i = 0; i < numVertices; i++)
+        {
+            for (int j = 0; j < numVertices; j++)
+            {
                 graph[i][j] = predefinedGraph[i][j];
             }
         }
-    } else if (option == 2) {
+    }
+    else if (option == 2)
+    {
         // Enter graph manually
         printf("Enter the number of vertices: ");
         scanf("%d", &numVertices);
 
         printf("Enter the adjacency matrix:\n");
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++) {
+        for (int i = 0; i < numVertices; i++)
+        {
+            for (int j = 0; j < numVertices; j++)
+            {
                 scanf("%d", &graph[i][j]);
             }
         }
-    } else if (option == 3) {
+    }
+    else if (option == 3)
+    {
         // Generate random graph
         srand(time(NULL)); // Seed random number generator with current time
 
         printf("Enter the number of vertices: ");
         scanf("%d", &numVertices);
 
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++) {
-                if (i == j) {
+        for (int i = 0; i < numVertices; i++)
+        {
+            for (int j = 0; j < numVertices; j++)
+            {
+                if (i == j)
+                {
                     graph[i][j] = 0; // No self-loops in the graph
-                } else {
+                }
+                else
+                {
                     graph[i][j] = rand() % 10; // Random weight between 0 and 9
                 }
             }
@@ -217,14 +249,15 @@ int main() {
     scanf("%d", &src);
 
     printf("Enter the destination vertex: ");
-    do {
+    do
+    {
         printf("Enter the destination vertex (must be smaller than %d): ", numVertices);
         scanf("%d", &dest);
     } while (dest >= numVertices);
 
     // Initialize SDL
-    SDL_Window* window;
-    SDL_Renderer* renderer;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
     // Set the blend mode to enable transparency
@@ -237,8 +270,10 @@ int main() {
 
     bool quit = false;
     SDL_Event event;
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
+    while (!quit)
+    {
+        while (SDL_PollEvent(&event))
+        {
             if (event.type == SDL_QUIT)
                 quit = true;
         }
